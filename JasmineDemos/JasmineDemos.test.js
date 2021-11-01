d4b662b8c73307e1dcf84c7a64cfa22df6d9ddf4
upluke <lukeforadream@gmail.com>
2021-11-01 10:35:02 -0700

    notes tobe vs toequal

--- JasmineDemos/JasmineDemos.test.js
+++ JasmineDemos/JasmineDemos.test.js
@@ -1,3 +1,9 @@
+// toBe() vs toEqual()
+// When comparing primitive types, toEqual() and toBe() will yield the same result. 
+// When comparing objects, toBe() is a stricter comparison, 
+// and if it is not the exact same object in memory this will return false. 
+// So unless you want to make sure it's the exact same object in memory, 
+// use toEqual() for comparing objects.
 describe('calculateTaxes tests', function(){
     it('should calculate lower-bracket taxes', function () {
         expect(calculateTaxes(10000)).toEqual(1500);
