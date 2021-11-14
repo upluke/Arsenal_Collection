d896df8c4833752c9093fe961c066674c4882624
upluke <lukeforadream@gmail.com>
2021-11-13 22:28:39 -0800

    meme form css updated

--- MemeGenerator/MemeGenerator.js
+++ MemeGenerator/MemeGenerator.js
@@ -4,10 +4,7 @@ let topTextInput=document.querySelector('input[id="top-text"]')
 let btmTextInput=document.querySelector('input[id="bottom-text"]')
 let display=document.querySelector("#display")
 
-
-
-
-
+// handle submit 
 form.addEventListener('submit', function(e){
   e.preventDefault()
   newMeme= createMeme(urlInput.value, topTextInput.value, btmTextInput.value)
@@ -16,28 +13,29 @@ form.addEventListener('submit', function(e){
   console.log(allMemes.childElementCount)
 
 })
-
+ // create a meme component
 function createMeme(url, top_text, btm_text){
- 
+  // container that holds image, top text and bottom text
   let container=document.createElement("div")
   container.setAttribute('class', 'container')
-
+  // image
   let img=document.createElement("img")
   img.src=url
   
+  // top text
   let top_div=document.createElement("div")
   top_div.setAttribute('class','top_div')
   top_div.innerText=top_text
-
+  // bottom text
   let btm_div=document.createElement("div")
   btm_div.setAttribute('class', 'btm_div')
   btm_div.innerText=btm_text
-
   
 
   container.appendChild(img)
   container.appendChild(top_div)
   container.appendChild(btm_div)
+ 
   
   // reset inputs
   urlInput.value=''
@@ -55,5 +53,5 @@ display.addEventListener('click', function(e){
  
 
 
-
+// url for testing:
 // https://thumbs.dreamstime.com/z/print-154745710.jpg
