0f26e22e692408bcc23f6d237f6f10f44150b795
upluke <lukeforadream@gmail.com>
2021-11-25 19:50:59 -0800

    find finindex exercise

--- advancedArrayMethods/FindFindIndex.js
+++ advancedArrayMethods/FindFindIndex.js
@@ -100,3 +100,69 @@ function myFindIndex(arr, callback) {
 myFindIndex(scores, function(score) {
 	return score !== 0 && score % 2 == 0;
 });
+
+
+
+
+// exercise:
+/* 
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
+findUserByUsername(users, 'taco') // undefined
+*/
+function findUserByUsername(usersArray, username) {
+	return usersArray.find(function(user){
+	  return user.username === username;
+	})
+  }
+//init:
+function findUserByUsername(usersArray, username) {
+	return usersArray.find(obj=>obj.username===username)
+  }
+  
+  const users = [
+	{username: 'mlewis'},
+	{username: 'akagen'},
+	{username: 'msmith'}
+  ];
+  console.log(findUserByUsername(users, 'taco'))
+  
+  
+  /*
+  Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+  
+  const users = [
+	{username: 'mlewis'},
+	{username: 'akagen'},
+	{username: 'msmith'}
+  ];
+  
+  removeUser(users, 'akagen') // {username: 'akagen'}
+  removeUser(users, 'akagen') // undefined
+  */
+  
+  function removeUser(usersArray, username) {
+	let foundIndex = usersArray.findIndex(function(user){
+	  return user.username === username;
+	})
+	if(foundIndex === -1) return;
+  
+	return usersArray.splice(foundIndex,1)[0];
+  }
+  //init:
+  function removeUser(usersArray, username) {
+	const idx= usersArray.findIndex(obj=>obj.username===username)
+	return usersArray[idx]
+  }
+  
+   
+  
+  
+  console.log(removeUser(users, 'akagen'))
