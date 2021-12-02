94a51bc41cdac5a7dcb51aee3cbd3e594f387481
upluke <lukeforadream@gmail.com>
2021-12-02 12:46:41 -0800

    object review

--- ES2015/ObjectEnhancements/ObjectEnhancements.js
+++ ES2015/ObjectEnhancements/ObjectEnhancements.js
@@ -1,36 +1,3 @@
-// We’ll start with a quick review of objects. In JS, we can make an object using object literal syntax with those curly braces. 
-//You may come across this term POJO (Plain Old JavaScript Object).
-const color='teal'
-
-const obj={}
-obj.color='#3723FF' // {color: "#3723FF"}
-obj[color]='#3723FF' // {color: "#3723FF", teal: "#3723FF"} 
-obj[1+4-2*8]='#3723FF'// {color: "#3723FF", teal: "#3723FF"; -11:"#3723FF"}  // it's evaluated to negative 11. 
-// Can add functions as keys:
-obj.sayHi = function() { return "Hi!" };
-obj.sayHi();   // Hi!
-//Can get arrays of keys, values, or [key, val] arrays:
-Object.keys(obj);    // ["color", "teal", "-11", "sayHi" ]
-
-Object.values(o1);  // ["#3723FF", "#3723FF", "#3723FF", function () {...} ]
-
-//entries gives you an array, where each element is a pair, a key-value pair 
-Object.entries(o1); // [["color", "#3723FF"], ["teal", "#3723FF"],["-11", "#3723FF"],
-                    //  ["sayHi", function () { ... } ]
-
-for (let [k, v] of Object.entries(obj)){
-    console.log(k, v)
-}
-
-
-// if you try to access a property that does not exist on an Object,
-// you don't get an error, you just get undefined.
-Object.bala; // undefined
-
-// All keys get "stringified": any time you add a key to an Object, it's turnned into a string.
-obj['-11'] // "#3723FF"
-// if we leave off those quotes, we still get the same match
-obj[-11] //"#3723FF"
 
 
 // Object Methods
---
+++ Object-Oriented Programming/ObjectOrientedProgramming.js
@@ -0,0 +1,70 @@
+// We’ll start with a quick review of objects. In JS, we can make an object using object literal syntax with those curly braces. 
+//You may come across this term POJO (Plain Old JavaScript Object).
+const color='teal'
+
+const obj={}
+obj.color='#3723FF' // {color: "#3723FF"}
+obj[color]='#3723FF' // {color: "#3723FF", teal: "#3723FF"} 
+obj[1+4-2*8]='#3723FF'// {color: "#3723FF", teal: "#3723FF"; -11:"#3723FF"}  // it's evaluated to negative 11. 
+// Can add functions as keys:
+obj.sayHi = function() { return "Hi!" };
+obj.sayHi();   // Hi!
+//Can get arrays of keys, values, or [key, val] arrays:
+Object.keys(obj);    // ["color", "teal", "-11", "sayHi" ]
+
+Object.values(o1);  // ["#3723FF", "#3723FF", "#3723FF", function () {...} ]
+
+//entries gives you an array, where each element is a pair, a key-value pair 
+Object.entries(o1); // [["color", "#3723FF"], ["teal", "#3723FF"],["-11", "#3723FF"],
+                    //  ["sayHi", function () { ... } ]
+
+for (let [k, v] of Object.entries(obj)){
+    console.log(k, v)
+}
+
+
+// if you try to access a property that does not exist on an Object,
+// you don't get an error, you just get undefined.
+Object.bala; // undefined
+
+// All keys get "stringified": any time you add a key to an Object, it's turnned into a string.
+obj['-11'] // "#3723FF"
+// if we leave off those quotes, we still get the same match
+obj[-11] //"#3723FF"
+
+// Mixing Data And Functionality
+// We can add functions as keys:
+/* area of right triangle */
+function getTriangleArea(a, b) {
+    return (a * b) / 2;
+  }
+  
+  /* hypotenuse of right triangle */
+  
+  function getTriangleHypotenuse(a, b) {
+    return Math.sqrt(a * a + b * b);
+  }
+  getTriangleArea(3, 4)          // 6
+  getTriangleHypotenuse(3, 4)    // 5
+
+// This gets a bit messy, though — all those functions to keep track of!
+// Using a POJO
+let triangle = {
+    a: 3,
+    b: 4,
+    getArea: function() {
+      return (this.a * this.b) / 2;
+    },
+    getHypotenuse: function() {
+      return Math.sqrt(this.a ** 2 + this.b ** 2);
+    }
+  };
+  triangle.getArea()          // 6
+  triangle.getHypotenuse()    // 5
+
+// this references to “this object”
+
+// So, we can helpfully mix data & functionality!
+
+// This is tidy: related functionality lives together
+// Annoying when we want more than one triangle
