214aaeace0cbd71d88a14ff120a139bf5d8603ad
upluke <lukeforadream@gmail.com>
2021-12-27 15:20:21 -0800

    set up funcs

--- ajax-giphy-party/ajax-giphy-party.js
+++ ajax-giphy-party/ajax-giphy-party.js
@@ -1,1 +1,14 @@
-console.log("Let's get this party started!");
+async function searchGifs(query){
+    const gifsData=axios.get('http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym')
+}
+
+function populateGifs(gifs){
+
+}
+
+
+$('#search-form').on('submit', async function handleSearch(evt){
+    evt.preventDefault()
+    console.log('click')
+    
+})
