23d8b1ed23a3787148ba175b69000b40b95625f0
upluke <lukeforadream@gmail.com>
2021-12-28 18:58:30 -0800

    populate gifs

--- ajax-giphy-party/ajax-giphy-party.js
+++ ajax-giphy-party/ajax-giphy-party.js
@@ -1,14 +1,37 @@
-async function searchGifs(query){
-    const gifsData=axios.get('http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym')
+// my api key: HWad2XWkwB1gSioV9jwV2QYAcXvt9suR
+async function searchGifs(query, limit){
+ 
+    const showData= await axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&limit=${limit.length===0?'1':limit}&api_key=HWad2XWkwB1gSioV9jwV2QYAcXvt9suR`)
+    
+    let result=[]
+    for (let sd of showData.data.data){
+      console.log(sd, "^^^^^^^^^^")
+      result.push({id: sd.id, url: sd.images.original.url})
+    }
+    return result 
 }
+
 
 function populateGifs(gifs){
+  console.log("inside", gifs)
+  const $gifsList = $('#gifs-list')
+  for (let gif of gifs){
+    $gifsList.append(`<img id=${gif.id} src=${gif.url}   alt=" " width="500" height="600"/>`)
+  }
 
+  // const $gif=$(`<img id=${gifs.id} res=${gifs} ult=  />`)
 }
+
+let selectOption=''
+$('#select-section').on('change', function(e){
+  selectOption= e.target.value
+})
 
 
 $('#search-form').on('submit', async function handleSearch(evt){
     evt.preventDefault()
-    console.log('click')
-    
+    const $searchQuery= $('#search-query').val()
+ 
+    const gifsData=  await searchGifs($searchQuery, selectOption)
+    populateGifs(gifsData)
 })
